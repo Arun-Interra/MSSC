1b4b9ca5ce0098ebed7f8a38f678a0da6e7b5360
akumar16 <cfs@mazdausa.com>
2022-07-08 15:04:28 -0700

    RO Data and Avg Visit Data Service and Repo
    Added the below items:
    Created Service and Repository For:
    RO Data
    Avg Visit Data

--- src/main/java/com/mazdausa/ssc/controller/BaseDataController.java
+++ src/main/java/com/mazdausa/ssc/controller/BaseDataController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mazdausa.ssc.dao.GenericResponse;
 import com.mazdausa.ssc.service.impl.GenericResponseWrapper;
 import com.mazdausa.ssc.service.impl.SscAltDataServiceImpl;
+import com.mazdausa.ssc.service.impl.SscAvgVisitDataServiceImpl;
+import com.mazdausa.ssc.service.impl.SscRoDataServiceImpl;
 
 @RestController
 @RequestMapping("/base-data")
@@ -16,6 +18,12 @@ public class BaseDataController {
 	
 	@Autowired
 	private SscAltDataServiceImpl altServ;
+	
+	@Autowired
+	private SscRoDataServiceImpl roServ;
+	
+	@Autowired
+	private SscAvgVisitDataServiceImpl avgVisitServ;
 	
 	@GetMapping("/alt/{dlrCD}")
 	public GenericResponse getDlrAltData(@PathVariable String dlrCD) {
@@ -27,5 +35,17 @@ public class BaseDataController {
 	public GenericResponse getAltData() {
 		return GenericResponseWrapper.GenericResponseFunction.apply(
 				altServ.getAltData(),null);
+	}
+	
+	@GetMapping("/avg-visit")
+	public GenericResponse getAvgVisitData() {
+		return GenericResponseWrapper.GenericResponseFunction.apply(
+				avgVisitServ.getAvgVistData(),null);
+	}
+	
+	@GetMapping("/ro")
+	public GenericResponse getRoData() {
+		return GenericResponseWrapper.GenericResponseFunction.apply(
+				roServ.getRoData(),null);
 	}
 }
